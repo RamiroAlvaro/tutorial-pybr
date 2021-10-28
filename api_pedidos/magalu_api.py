07ce590bde496e11d8eba40e89e16e67eddc34a5
RamiroAlvaro <ramiroalvaro@hotmail.com>
2021-10-28 19:25:12 -0300

    Adiciona listagem de itens em um pedido

--- api_pedidos/magalu_api.py
+++ api_pedidos/magalu_api.py
@@ -1,26 +1,16 @@
+from http import HTTPStatus
 import os
-
-import httpx
-from fastapi import FastAPI, Depends, Request
 from uuid import UUID
 from api_pedidos.esquema import Item
-from fastapi.responses import JSONResponse
-from http import HTTPStatus
 from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
 
-app = FastAPI()
+import httpx
 
 # tenant e apikey fixos somente para demonstrações
 APIKEY = os.environ.get("APIKEY", "5734143a-595d-405d-9c97-6c198537108f")
 TENANT_ID = os.environ.get("TENANT_ID", "21fea73c-e244-497a-8540-be0d3c583596")
 MAGALU_API_URL = "http://localhost:8080"
 MAESTRO_SERVICE_URL = f"{MAGALU_API_URL}/maestro/v1"
-
-
-@app.get("/healthcheck")
-async def healthcheck():
-    return {"status": "ok"}
-
 
 def _recupera_itens_por_pacote(uuid_do_pedido, uuid_do_pacote):
     response = httpx.get(
@@ -62,21 +52,3 @@ def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
         raise exc
     except httpx.HTTPError as exc:
         raise FalhaDeComunicacaoError() from exc
-
-
-@app.get("/orders/{identificacao_do_pedido}/items")
-def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
-    return itens
-
-
-@app.exception_handler(PedidoNaoEncontradoError)
-def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
-    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
-
-
-@app.exception_handler(FalhaDeComunicacaoError)
-def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
-    return JSONResponse(
-        status_code=HTTPStatus.BAD_GATEWAY,
-        content={"message": "Falha de comunicação com o servidor remoto"}
-    )
